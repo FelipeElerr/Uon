fe3878e0c72269109cbe48389a6537e86ac97ce9
Felipe Eler <200738@facens.br>
2022-11-28 00:07:39 -0300

    Deletando Branch

--- API/src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-
-mongoose.connect("mongodb+srv://Rafael:rafael123@devweb.zvroq.mongodb.net/?retryWrites=true&w=majority");
-
-let db = mongoose.connection;
-
-export default db;
--- WEB/chamada_automatica/src/history.js
+++
@@ -1,9 +0,0 @@
-/*
-Esta biblioteca aplica algumas funções de navegação baseado no historico de paginas
-*/
-
-import { createBrowserHistory } from 'history'
-
-const history = createBrowserHistory()
-
-export { history }
